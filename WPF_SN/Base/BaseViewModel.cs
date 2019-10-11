eb8e46b8cc0ace3bf8a8750d12e94b4a6d6db0a9
f0cuS <f0cuSmcsza@gmail.com>
2019-10-11 22:36:04 +0300

    Initial design

---
+++ WPF_SN/Base/BaseViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPF_SN.Base
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+
+
+        public class Command : ICommand
+        {
+            public delegate void Del();
+            Del del;
+
+            public Command(Del del)
+            {
+                this.del = del;
+            }
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+            public void Execute(object parameter)
+            {
+                del();
+            }
+
+        }
+    }
+}
---
+++ WPF_SN/MainWindowViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+using WPF_SN.ChatContacts;
+using WPF_SN.ChatLog;
+
+
+namespace WPF_SN
+{
+    class MainWindowViewModel : Base.BaseViewModel
+    {
+        //public ObservableCollection<ChatContactNode> contacts { get; set; }
+
+        //public ObservableCollection<ChatLogNode> chatList { get; set; }
+
+        //public Command SendMsgToChat { get; set; }
+        //public Command keyPress { get; set; }
+
+
+       // public string userMsg { get; set; }
+
+
+        public MainWindowViewModel()
+        {
+            //contacts = ChatContactsModel.getInstance().getContacts();
+            //setupCommands();
+        }
+
+        //private string _correspondenceNickName;
+        //public ChatContactNode correspondenceNickName
+        //{
+        //    set
+        //    {
+        //        _correspondenceNickName = value.contactName;
+        //        if (value != null)
+        //        {
+        //            chatList = ChatLogModel.getInstance().getChat(_correspondenceNickName);
+        //            OnPropertyChanged(nameof(chatList));
+        //        }
+
+        //    }
+        //}
+
+        private void setupCommands()
+        {
+            //SendMsgToChat = new Command(() =>
+            //{
+            //    if (this._correspondenceNickName == null)
+            //    {
+            //        MessageBox.Show("Select User to send meesage");
+            //        return;
+            //    }
+            //    ChatLogModel.getInstance().addMessageToLog(_correspondenceNickName, "It's me", userMsg);
+
+
+            //    if (userMsg != "")
+            //    {
+            //        string[] words = userMsg.Split();
+            //        foreach (string word in words)
+            //        {
+            //            // TODO доработать логику выбора ответа
+            //            ObservableCollection<string> Answers = ChatBotModel.getInstance().getAnswers(word);
+            //            if (Answers.Count < 1)
+            //            {
+            //                ChatLogModel.getInstance().addMessageToLog(_correspondenceNickName, _correspondenceNickName, "Я тебя не понимаю, напиши что-то годное!");
+            //            }
+            //            else
+            //            {
+            //                ChatLogModel.getInstance().addMessageToLog(_correspondenceNickName, _correspondenceNickName, Answers[1]);
+            //            }
+            //        }
+
+            //        chatList = ChatLogModel.getInstance().getChat(_correspondenceNickName);
+            //        OnPropertyChanged(nameof(chatList));
+
+            //        userMsg = "";
+            //        OnPropertyChanged(nameof(userMsg));
+            //    }
+
+            //});
+
+            //keyPress = new Command(() =>
+            //{
+            //    if (this._correspondenceNickName == null)
+            //    {
+            //        MessageBox.Show("Select User to send meesage");
+            //        return;
+            //    }
+            //    ChatLogModel.getInstance().addMessageToLog(_correspondenceNickName, "It's me", userMsg);
+
+
+            //    if (userMsg != "")
+            //    {
+            //        string[] words = userMsg.Split();
+            //        foreach (string word in words)
+            //        {
+            //            // TODO доработать логику выбора ответа
+            //            ObservableCollection<string> Answers = ChatBotModel.getInstance().getAnswers(word);
+            //            if (Answers.Count < 1)
+            //            {
+            //                ChatLogModel.getInstance().addMessageToLog(_correspondenceNickName, _correspondenceNickName, "Я тебя не понимаю, напиши что-то годное!");
+            //            }
+            //            else
+            //            {
+            //                ChatLogModel.getInstance().addMessageToLog(_correspondenceNickName, _correspondenceNickName, Answers[1]);
+            //            }
+            //        }
+
+            //        chatList = ChatLogModel.getInstance().getChat(_correspondenceNickName);
+            //        OnPropertyChanged(nameof(chatList));
+
+            //        userMsg = "";
+            //        OnPropertyChanged(nameof(userMsg));
+            //    }
+
+            //});
+
+        }
+
+
+
+        private void TBmsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (e.Key == Key.Enter)
+            //{
+            //    //ButtonSendMessage.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+
+            //}
+
+        }
+    }
+}
